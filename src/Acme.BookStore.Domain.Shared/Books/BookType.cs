02201a2859e1d544552a49ef0712335fc4c20c67
Josue Fuentes <68027721+josuefuentesdev@users.noreply.github.com>
2023-01-13 18:36:55 -0500

    Add project files.

---
+++ src/Acme.BookStore.Domain.Shared/Books/BookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Acme.BookStore.Books
+{
+    public enum BookType
+    {
+        Undefined,
+        Adventure,
+        Poetry,
+        Dystopia,
+        ScienceFiction
+    }
+}
